ca5ad37007a5955d1a84e09ffebc9a43d1292d2a
andryandroid <andryandroid@users.noreply.github.com>
2019-11-25 18:14:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+var frase = {
+    who: ['the dog','my granma','his turtle','my bird'],
+    what: [ 'ate','pissed','crashed','broked'],
+    object: ['my homework','my car','my sandwich','my computer'],
+    when: [ 'before the class','this morning','when I finished it','during my lunch','while I was praying']
+    };
 
+function random(max){
+    var num = Math.floor(Math.random()*max);
+    return num;
+}
+
+window.onload=function (){
+document.querySelector('#excuse').innerHTML= frase.who[random(frase.who.length)]+" "+ frase.what[random(frase.what.length)]+" "+frase.object[random(frase.object.length)]+" "+frase.when[random(frase.when.length)];
+}
